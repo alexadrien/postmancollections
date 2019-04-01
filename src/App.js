fa2fcac3c63ce0a5a085c5da818f9a70510a54e6
alexadrien <alex.adrien.auger@gmail.com>
2019-04-01 14:06:06 +0200

    list

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
 import AddIcon from '@material-ui/icons/Add';
 import Text from "./components/Text";
+import data from "./data";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
 class App extends Component {
+  handleCollectionClick = collection => () => {
+    window.location  = `https://raw.githubusercontent.com/alexadrien/postmancollections/master/data/${collection.file}`;
+  };
+
   render() {
     return (
       <div>
@@ -19,6 +27,15 @@ class App extends Component {
             </Button>
           </Toolbar>
         </AppBar>
+        <div>
+          <List>
+            {data.collections.map(collection => (
+              <ListItem button onClick={this.handleCollectionClick(collection)}>
+                <ListItemText primary={collection.name} />
+              </ListItem>
+            ))}
+          </List>
+        </div>
       </div>
     );
   }
