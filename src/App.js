034e7a40ef0a7db052960cb86fd11ddf8babea33
alexadrien <alex.adrien.auger@gmail.com>
2019-04-01 13:53:32 +0200

    icon

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import MenuIcon from '@material-ui/icons/Menu';
+import AddIcon from '@material-ui/icons/Add';
+import Text from "./components/Text";
 
 class App extends Component {
   render() {
@@ -15,10 +16,10 @@ class App extends Component {
             <IconButton color="inherit" aria-label="Menu">
               <MenuIcon />
             </IconButton>
-            <Typography variant="h6" color="inherit">
+            <Text variant="h6" color="inherit" grow>
               News
-            </Typography>
-            <Button color="inherit">Login</Button>
+            </Text>
+            <Button color="inherit"><AddIcon /></Button>
           </Toolbar>
         </AppBar>
       </div>
